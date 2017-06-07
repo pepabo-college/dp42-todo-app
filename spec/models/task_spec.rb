602af4dd9d64ba56a1d4d2b6a8afbe3116fbe76b
Koichi Sato <ksato@pepabo.com>
2017-06-07 16:40:42 +0900

    taskモデルのテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'content' do
+    context '空白のとき' do
+      it 'valid でないこと' do
+        task = Task.new(content: '', status: 0)
+        task.valid?
+        expect(task.errors[:content]).to be_present
+      end
+    end
+  end
 end
