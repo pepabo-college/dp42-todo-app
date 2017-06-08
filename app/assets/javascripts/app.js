35d586971f8e0bf6ceb2d3e737ca96bd47a38a9f
Koichi Sato <ksato@pepabo.com>
2017-06-08 17:11:47 +0900

    webpackを追加

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,1 @@
+import jpg from '../images/test.jpg'
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './app/assets/javascripts/app.js',
+  output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'app/assets/javascripts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
