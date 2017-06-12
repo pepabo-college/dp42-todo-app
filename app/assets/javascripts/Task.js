ae38f432ae398bf98fd46fee9ce5713a51e7b91c
Koichi Sato <ksato@pepabo.com>
2017-06-12 18:40:02 +0900

    ダブルクリックでTODO編集の入力フォームを表示

--- app/assets/javascripts/Task.js
+++ app/assets/javascripts/Task.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default class Task extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {isEditable: false, editingText: ''};
+  }
+
   handleDelete(e) {
     e.preventDefault();
     this.props.onTaskDelete({id: this.props.id});
@@ -10,13 +16,23 @@ export default class Task extends React.Component {
     e.preventDefault();
     this.props.onTaskUpdate({task: {id: this.props.id, status: e.target.value}});
   }
+  handleDoubleClick(e) {
+    e.preventDefault();
+    this.setState({ isEditable: true ,editingText: this.props.content});
+    console.log(this.state.isEditable);
+  }
 
   render() {
     return(
       <tr key={this.props.id}>
-        <td>
+        <td onDoubleClick={this.handleDoubleClick.bind(this)}>
           {this.props.content}
         </td>
+        {this.state.isEditable ? 
+        <td >
+          <input type="text" defaultValue={this.state.editingText}></input>
+        </td>
+        : null}
         <td>
         <select defaultValue={this.props.status} onChange={this.handleUpdate.bind(this)}>
           <option value="todo" key="todo">todo</option>
