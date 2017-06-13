30ece95fb3baa790b977982491c56570eb45967e
Koichi Sato <ksato@pepabo.com>
2017-06-13 10:14:10 +0900

    フォームのトグルとtodoの登録を実装

--- app/assets/javascripts/Task.js
+++ app/assets/javascripts/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class Task extends React.Component {
 
@@ -6,35 +7,61 @@ export default class Task extends React.Component {
     super();
     this.state = {isEditable: false, editingText: ''};
   }
-
+  componentDidUpdate() {
+    let editing = ReactDOM.findDOMNode(this.refs.editing);
+    editing && editing.focus();
+  }
   handleDelete(e) {
     e.preventDefault();
     this.props.onTaskDelete({id: this.props.id});
   }
 
-  handleUpdate(e) {
+  handleStatusUpdate(e) {
     e.preventDefault();
     this.props.onTaskUpdate({task: {id: this.props.id, status: e.target.value}});
   }
   handleDoubleClick(e) {
     e.preventDefault();
     this.setState({ isEditable: true ,editingText: this.props.content});
-    console.log(this.state.isEditable);
+  }
+  handleSubmitEdit(e) {
+    e.preventDefault();
+    this.props.onTaskUpdate({task: {id: this.props.id, content: e.target.editingForm.value}});
+    this.setState({ isEditable: false });
+  }
+
+  handleBlurEdit(e) {
+    e.preventDefault();
+    this.props.onTaskUpdate({task: {id: this.props.id, content: e.target.value}});
+    this.setState({ isEditable: false });
+  }
+
+  showEditingForm(){
+      return( 
+        <td >
+          <form onSubmit={this.handleSubmitEdit.bind(this)}>
+          <input type="text" name="editingForm" ref="editing" defaultValue={this.state.editingText} 
+            onBlur={this.handleBlurEdit.bind(this)}>
+          </input>
+        </form>
+        </td>
+        );
+    }
+
+  showTodo(){
+    return(
+      <td onDoubleClick={this.handleDoubleClick.bind(this)}>
+        {this.props.content}
+      </td>
+    );
   }
 
   render() {
     return(
       <tr key={this.props.id}>
-        <td onDoubleClick={this.handleDoubleClick.bind(this)}>
-          {this.props.content}
-        </td>
-        {this.state.isEditable ? 
-        <td >
-          <input type="text" defaultValue={this.state.editingText}></input>
-        </td>
-        : null}
+        {this.state.isEditable ? this.showEditingForm() : this.showTodo()}
         <td>
-        <select defaultValue={this.props.status} onChange={this.handleUpdate.bind(this)}>
+        <select defaultValue={this.props.status} onChange={this.handleStatusUpdate.bind(this)}>
           <option value="todo" key="todo">todo</option>
           <option value="doing" key="doing">doing</option>
           <option value="done" key="done">done</option>
