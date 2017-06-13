95e0c190a9e2ac2a5d54ff2dc9999f7955063a53
Koichi Sato <ksato@pepabo.com>
2017-06-13 17:57:42 +0900

    createのテストを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    
+
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
   describe 'GET #index' do
-    
+
     context 'ログインしていない状態でアクセスしたとき' do
       before do
         get :index
@@ -54,6 +54,42 @@ RSpec.describe TasksController, type: :controller do
 
       it 'ステータスコード200を返すこと' do
         expect(response.status).to eq(200)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'ログインユーザーでない場合' do
+      it 'ステータスコード302を返すこと' do
+        post :create, params: {task: FactoryGirl.attributes_for(:task)}
+        expect(response.status).to eq(302)
+      end
+
+      it 'データベースに新しいTODOが登録できないこと' do
+        expect{
+          post :create, params: {task: FactoryGirl.attributes_for(:task)}
+        }.to change(Task, :count).by(0)
+      end
+
+      it 'ログインページにリダイレクトされること'
+    end
+
+    context 'ログインユーザーの場合' do
+      login_user
+      it 'ステータスコード302を返すこと' do
+        post :create, params: {task: FactoryGirl.attributes_for(:task)}
+        expect(response.status).to eq(302)
+      end
+
+      it 'データベースに新しいTODOが登録されること' do
+        expect{
+          post :create, params: {task: FactoryGirl.attributes_for(:task)}
+        }.to change(Task, :count).by(1)
+      end
+
+      it 'タスクページにリダイレクトされること' do
+        post :create, params: {task: FactoryGirl.attributes_for(:task)}
+        expect(response).to redirect_to(task_path(assigns(:task)))
       end
     end
   end
