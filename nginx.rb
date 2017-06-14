0ef674240c4373d4683aa9d3a5e6a81769877bd8
Koichi Sato <ksato@pepabo.com>
2017-06-14 14:58:21 +0900

    Itamaeのためのnginx.rbを作成

---
+++ nginx.rb
@@ -0,0 +1,43 @@
+execute 'install bundle gems' do
+  command 'bundle install'
+  cwd '/var/www/todo'
+
+end
+
+execute 'migrate database' do
+  command 'bin/rake db:migrate'
+  cwd '/var/www/todo'
+end
+
+execute 'install npm modules' do
+  command <<-EOS
+    npm install
+    npm run build
+  EOS
+  user 'vagrant'
+  cwd '/var/www/todo'
+end
+
+service 'todo_app' do
+  action [:start, :enable]
+end
+
+package 'nginx'
+service 'nginx' do
+  action [:start, :enable]
+end
+
+file '/etc/nginx/conf.d/prxy.conf' do
+  content <<-EOS
+  server {
+    listen 80;
+    server_name wdp4.com;
+    location {
+      proxy_pass http://192.168.80.10:3000;
+    }
+  }
+  EOS
+  owner 'nginx'
+  group'nginx'
+  notifies :reload, 'service[nginx]'
+end
